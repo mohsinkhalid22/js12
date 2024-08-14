0743be810c135b9206f0457aca476c989b6a50a3
mohsinkhalid22 <mohsinu2gud@gmail.com>
2024-08-14 17:23:26 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// console.dir(document.head);
+// console.log(document.body);
+
+// document.body.childNodes[1].innerText = "abcd";
+
+let headings = document.getElementsByClassName("heading-class");
+console.dir(headings);
+console.log(headings);
