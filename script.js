e46f739d3d125b2736053a1745111e42e7998423
mohsinkhalid22 <mohsinu2gud@gmail.com>
2024-08-15 21:48:02 +0500

    dom

--- script.js
+++ script.js
@@ -2,7 +2,18 @@
 // console.log(document.body);
 
 // document.body.childNodes[1].innerText = "abcd";
+let button = document.getElementById("myId");
+console.dir(button);
 
-let headings = document.getElementsByClassName("heading-class");
+let headings = document.getElementsByClassName("myClass");
 console.dir(headings);
 console.log(headings);
+
+let parahs = document.getElementsByTagName("p");
+console.dir(parahs);
+
+let firstEl = document.querySelector("h1");
+console.dir(firstEl);
+
+let allEl = document.querySelectorAll;
+console.dir(allEl);
