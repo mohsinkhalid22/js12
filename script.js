1fcefe336e90f48f8ba3f8577f7ad30abe6ebd13
mohsinkhalid22 <mohsinu2gud@gmail.com>
2024-08-22 14:16:21 +0500

    final

--- script.js
+++ script.js
@@ -20,3 +20,22 @@ console.dir(allEl);
 
 let div = document.querySelector("div");
 console.log(div);
+
+let head = document.querySelector("h4");
+
+let h2 = document.querySelector("h2");
+
+console.dir(h2.innerText);
+
+h2.innerText = h2.innerText + " from Apna College.";
+
+let divs = document.querySelectorAll(".box");
+
+let idx = 1;
+
+for (div of divs){
+    div.innerText = `new unique value ${idx}`;
+    idx++;
+}
+// console.log(div);
+
